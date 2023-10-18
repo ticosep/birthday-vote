294e9b195cb0a8a548e1d8afbd704dc7e3b9cb91
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-10-18 17:09:35 -0300

    feat: add candidates list and image

--- src/components/CandidatesList.tsx
+++ src/components/CandidatesList.tsx
@@ -1,7 +1,19 @@
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { Box, CircularProgress } from '@mui/material';
+import {
+    Box,
+    CircularProgress,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    FormControl,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
+} from '@mui/material';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { fetchCandidates } from '../features/candidates';
 import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CandidatesList = () => {
     const dispatch = useAppDispatch();
@@ -28,9 +40,60 @@ const CandidatesList = () => {
                     <h3>
                         Não tente roubar, nós temos um sistema de auditoria{' '}
                     </h3>
-                    {Object.entries(candidates).map(([a, b]) => (
-                        <span>{a}</span>
-                    ))}
+                    <form>
+                        <Box width="100%">
+                            <FormControl>
+                                <RadioGroup>
+                                    {Object.entries(candidates).map(
+                                        ([name]) => (
+                                            <Accordion
+                                                sx={{ my: 2 }}
+                                                key={name}
+                                            >
+                                                <AccordionSummary
+                                                    style={{
+                                                        pointerEvents: 'none',
+                                                    }}
+                                                    expandIcon={
+                                                        <FontAwesomeIcon
+                                                            style={{
+                                                                pointerEvents:
+                                                                    'auto',
+                                                            }}
+                                                            icon={faCamera}
+                                                        />
+                                                    }
+                                                >
+                                                    <FormControlLabel
+                                                        style={{
+                                                            pointerEvents:
+                                                                'auto',
+                                                        }}
+                                                        value={name}
+                                                        control={<Radio />}
+                                                        label={name}
+                                                    />
+                                                </AccordionSummary>
+                                                <AccordionDetails>
+                                                    <img
+                                                        src="https://plus.unsplash.com/premium_photo-1661894950761-312d659007d3?auto=format&fit=crop&q=80&w=1420&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                                        alt={name}
+                                                        loading="lazy"
+                                                        style={{
+                                                            height: '100%',
+                                                            width: '100%',
+                                                            objectFit:
+                                                                'contain',
+                                                        }}
+                                                    />
+                                                </AccordionDetails>
+                                            </Accordion>
+                                        ),
+                                    )}
+                                </RadioGroup>
+                            </FormControl>
+                        </Box>
+                    </form>
                 </>
             )}
         </Box>
